c9fc88473c6e5ad656fd5fffc68922e700001610
Newton Ombese <newtonombese1@gmail.com>
2023-02-21 11:15:09 +0300

    :art:feat: Data Types in Dart Technical Assignment

---
+++ assignment-1.dart
@@ -0,0 +1,21 @@
+void main(){
+  // working with int
+  int counties = 47;
+  print(counties);
+
+  // working with string
+  String name = "Newton";
+  print(name);
+
+  // working with double
+  double averageMean = 8.72;
+  print(averageMean);
+
+  // working with List
+  List places = ['Highrise', 'Soweto', 'Lindi', 'Mashimoni'];
+  print(places);
+
+  // working with Map
+  var details = {'Username':'Newton','Password':'pass@123'}; 
+  print(details); 
+}
