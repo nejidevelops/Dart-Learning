39013ca532f41da53f054ef55cf67937cde2eee9
Newton Ombese <newtonombese1@gmail.com>
2023-03-08 14:05:16 +0300

    :art:feat: Learning OOP in dart

---
+++ Object-Oriented-Programming.dart
@@ -0,0 +1,41 @@
+// OOP In Dart
+
+// Object-oriented programming (OOP) is a programming method that uses objects and their interactions to design and program applications. It is one of the most popular programming paradigms and is used in many programming languages, such as Dart, Java, C++, Python, etc.
+
+// In OOP, an object can be anything, such as a person, a bank account, a car, or a house. Each object has its attributes (or properties) and behavior (or methods). For example, a person object may have the attributes name, age and height, and the behavior walk and talk.
+
+// Advantages
+// It is easy to understand and use.
+// It increases reusability and decreases complexity.
+// The productivity of programmers increases.
+// It makes the code easier to maintain, modify and debug.
+// It promotes teamwork and collaboration.
+// It reduces the repetition of code.
+
+// Features Of OOP
+// Class
+// Object
+// Encapsulation
+// Inheritance
+// Polymorphism
+// Abstraction
+
+// Key Points
+// Object Oriented Programming (OOP) is a programming paradigm that uses objects and their interactions to design and program applications.
+// OOP is based on objects, which are data structures containing data and methods.
+// OOP is a way of thinking about programming that differs from traditional procedural programming.
+// OOP can make code more modular, flexible, and extensible.
+// OOP can help you to understand better and solve problems.
+
+
+// Classes
+// Nearly all the code you write in Dart will be contained in classes. And a class is a blueprint for an object. That is, a class describes an object that you can create. The object itself is what holds any specific data and logic. For example, a Cat class might look like this:
+// The variable declarations above, String name and String color, are called properties or class members. (NB: Members can also refer to functions, getters and setters on classes.)
+
+// Instances of classes
+// This class describes an object that can be created, like this:
+
+// A note about the (lack of) the new keyword: ` If you're coming from many other object-oriented languages, you've probably seen the `new` keyword used to create new instances of a class. In Dart, this `new` keyword works the same way, but _it isn't necessary_. In Dart 2, you don't need to use `new` or `const` to create an object. The compiler will infer that for you.
+
+
+
