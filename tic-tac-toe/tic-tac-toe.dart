4c58c9486d60d827100569f0ff221d3968831607
Newton Ombese <newtonombese1@gmail.com>
2023-03-28 17:52:43 +0300

    :art:feat: Creation of a function that creates the board and main that calls the function

--- tic-tac-toe/tic-tac-toe.dart
+++ tic-tac-toe/tic-tac-toe.dart
@@ -6,7 +6,21 @@ bool isXturn = true;
 int moveCount = 0;
 
 List<String> values = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
-
 // |1|2|3|
 // |4|5|6|
 // |7|8|9|
+List<String> combinations = ['012', '048', '036', '147', '246', '345', '678'];
+
+void generateBoard(){
+  print('   |   |   ');
+  print(' ${values[0]} | ${values[1]} | ${values[2]}');
+  print('   |   |   ');
+  print(' ${values[3]} | ${values[4]} | ${values[5]}');
+  print('   |   |   ');
+  print(' ${values[6]} | ${values[7]} | ${values[8]}');
+  print('   |   |   ');
+}
+
+void main(){
+  generateBoard()
+}
