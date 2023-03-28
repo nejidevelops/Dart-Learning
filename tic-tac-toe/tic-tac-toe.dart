c59844781e66c35876b9a3615efb9ff408bebfea
Newton Ombese <newtonombese1@gmail.com>
2023-03-28 18:07:48 +0300

    :art::tada: Done with the tic tac toe game

--- tic-tac-toe/tic-tac-toe.dart
+++ tic-tac-toe/tic-tac-toe.dart
@@ -11,6 +11,34 @@ List<String> values = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
 // |7|8|9|
 List<String> combinations = ['012', '048', '036', '147', '246', '345', '678'];
 
+bool checkCombination(String combination, String checkFor){
+  List<int> numbers = combination.split('').map((item){
+    return int.parse(item);
+  }).toList();
+  bool match = false;
+  for (final item in numbers){
+    if (values[item] == checkFor){
+      match = true;
+    }
+    else {
+      match = false;
+      break;
+    }
+  }
+  return match;
+}
+
+void checkWinner(player){
+  for(final item in combinations){
+    bool combinationValidity = checkCombination(item, player);
+    if(combinationValidity == true){
+      print('$player WON!!!');
+      winner = true;
+      break;
+    }
+  }
+}
+
 void generateBoard(){
   print('   |   |   ');
   print(' ${values[0]} | ${values[1]} | ${values[2]}');
@@ -21,6 +49,26 @@ void generateBoard(){
   print('   |   |   ');
 }
 
+void getnextCharacter(){
+  print('Choose number for ${isXturn == true ? "X" : "O"}');
+  int number = int.parse(stdin.readLineSync());
+  values[number - 1] = isXturn ? 'X' : 'O';
+  isXturn = !isXturn;
+  moveCount++;
+  if(moveCount == 9){
+    winner = true;
+    print("DRAW!");
+  }
+  else {
+    clearScreen();
+    generateBoard();
+
+  checkWinner('X');
+  checkWinner('O');
+
+  if(winner == false) getnextCharacter();
+}
 void main(){
-  generateBoard()
+  generateBoard();
+  getnextCharacter();
 }
