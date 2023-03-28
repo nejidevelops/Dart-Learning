e3fba83b4407b26b42d9dcb3866805fa28e20f97
Newton Ombese <newtonombese1@gmail.com>
2023-03-28 17:37:02 +0300

    :art::tada: Adding the two functions for numInput and operandChoice

--- calculator-app/calculator.dart
+++ calculator-app/calculator.dart
@@ -3,5 +3,12 @@ import "dart:math";
 
 double numInput(){
   print("Enter the number for operation");
-  double myNumber = double.parse();
+  double myNumber = double.parse(stdin.readLineSync()!);
+  return myNumber;
+}
+
+String operandChoice(){
+  print("Enter the operand e.g '+', '-', '/','*'");
+  String operandInput = stdin.readLineSync()!;
+  return operandInput;
 }
