0d63a82c2e29f35e6d4a25bb46a59d353754a55f
Newton Ombese <newtonombese1@gmail.com>
2023-03-28 17:39:23 +0300

    :art:feat: Working on the operations using switch cases

--- calculator-app/calculator.dart
+++ calculator-app/calculator.dart
@@ -12,3 +12,27 @@ String operandChoice(){
   String operandInput = stdin.readLineSync()!;
   return operandInput;
 }
+
+void main(){
+  double num1 = numInput();
+  double num2 = numInput();
+  String operand = operandChoice();
+  double result = 0;
+  switch(operand){
+    case "+":
+      result = num1 + num2;
+      break;
+    case "-":
+      result = num1 - num2;
+      break;
+    case "*":
+      result = num1 * num2;
+      break;
+    case "/":
+      result = num1 / num2;
+      break;
+    default:
+      print("Invalid input");
+      break;
+  }
+}
